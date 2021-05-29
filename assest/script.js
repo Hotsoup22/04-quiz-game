e57ba2f825c0a93c4929a0e8a9258d6dc41376d9
hotsoup42 <jose22gw@gmail.com>
2021-05-28 20:41:56 -0700

    added css block elemnt to newly created `hiden classes`
    created containers for my sections, i replaced some sections or wrapped them in divs`container
    
    added global var to js

---
+++ assest/script.js
@@ -0,0 +1,44 @@
+//global var
+var startButton = document.querySelector('#startButton') ;
+var startGameContainer = document.querySelector('#startGameContainer');
+var
+
+
+//startButton function
+function startGame(){
+    var
+}
+
+
+// DEclare a starting 'score`
+
+//Declare an array list of 'questions'
+    //WHERE each value is an object to define a question and its properties 
+
+//DECLARE an `questionIndex` to point to the current question
+
+// Store a reference to the element to the 'timerDisplayEl'
+//Store a refrence to the 'StartButtonEl'
+
+//create 'stateGame' function
+
+    // call 'startTimer`
+    //initializing the string 'timeleft' value
+    //start the running countdown on the timer
+    //hide the start screen element and show the first question
+// create `endGmae` function
+//create `startTimer` Function
+
+    //initializing the starting 'score' value
+    //start the running countdown on the `score`
+    //Hide the start screen element and show the first question
+
+// Create `answerQuestion` function 
+    //add button listner
+    //get the value associate with the clicked `button`
+    //compare it against the correct answer for the current question
+        //if incorrect...
+            //subract points from the `score`   
+    //continue to the next question   `renderCurrentQuestion()`
+// Create `renderCurrentQuestion` function 
+    // access the current question data from questions[questionIndex]
