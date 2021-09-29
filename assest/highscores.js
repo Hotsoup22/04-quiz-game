d623614a7e8e2d03a65c6494f5553b14a0f70378
hotsoup42 <jose22gw@gmail.com>
2021-09-29 16:03:40 -0700

    header around `nav` html tag

--- assest/highscores.js
+++ assest/highscores.js
@@ -10,18 +10,15 @@ var initialsBtn = document.getElementById('#buttonInitials');
 console.log(highscores);
 console.log(initials);
 function showHighScore(){
+  storedHighscores.sort( function( a, b ) {
+    var answerA = a.answerScore;
+    var timeA = a.timeScore;
+    var answerB = b.answerScore;
+    var timeB = b.timeScore;
+  
+    return ( answerA < answerB ? 1 : ( answerA > answerB ? -1 : ( answerA === answerB ? ( timeA < timeB ? 1 : ( timeA > timeB ? -1 : 0 ) ) : null ) ) );
+  } );
 
-
-      initials = initals.value;
-    
      
-   
-    // console.log
-     var li  = document.createElement("li");
-     const scores = document.querySelector(".win-loss-container")
-    // var words = document.createTextNode(highscores)  ; //content of p
-     scores.appendChild(li);
-     li.appendChild(initials);
-    // winLossContainer.append(words);
   
 }
