74e9fdc34f88a8b440a905071d57e165266dda04
hotsoup42 <jose22gw@gmail.com>
2023-05-10 18:54:56 -0700

    added new questions with correct answers. Changed time remaining from 30sec to 60sec. Added classes for styling purposes where needed. Highscore array with limit of 5 items was created. set user score and name with local storage.

--- assest/script2.js
+++ assest/script2.js
@@ -1,11 +1,9 @@
-// var startGameBtn = $('#start') ;
-// var restartBtn = $(".restart");
 var win = document.querySelector(".correctA");
 var lose = document.querySelector(".lose");
 var timerElement = document.querySelector(".timer-count");
 var startButton = document.querySelector(".start-button");
-
-
+var results = document.querySelector(".results")
+var answersBtn = document.querySelectorAll(".answersBtn");
 
 // var questionsTitle = $('.questionsTitle');
 var questionsTitle = document.getElementById("questionsTitle");
@@ -14,49 +12,57 @@ var btn1 = document.getElementById("btn1");
 var btn2 = document.getElementById("btn2");
 var btn3 = document.getElementById("btn3");
 var btn4 = document.getElementById("btn4");
-var answersBtn = document.querySelectorAll(".answersBtn");
+
 var answersContainerDiv = $("#answersContainer");
 let currentQuestionIndex = 0;
-let winCounter ;
+let winCounter;
 let timerCount;
+let highscores = JSON.parse(localStorage.getItem('highscores')) || []
+const btnbyId = [btn1, btn2, btn3, btn4]
+btn1.disabled = true;
+btn2.disabled = true;
+btn3.disabled = true;
+btn4.disabled = true;
 startButton.addEventListener("click", startGame);
-//
 // The startGame function is called when the start button is clicked
 function startGame() {
-  $(".hidden").css({ display: "block" });
   $("#buttonInitials").css({ display: "none" });
   $("#startTitle").css({ display: "none" });
+  $(".game_description").css({ display: "none" });
+  results.classList.remove("hidden");
+  //enable buttons
+  btn1.disabled = false;
+  btn2.disabled = false;
+  btn3.disabled = false;
+  btn4.disabled = false;
   isWin = false;
-  timerCount = 30;
+  timerCount = 60;
   startButton.disabled = true;
-  winCounter=0;
+  startButton.classList.add("hidden")
+  winCounter = 0;
   renderQuestions();
   startTimer();
 }
 
-
 // Updates the timer value to immediate value
 function updateTimerValue() {
-  timerElement.textContent = timerCount;
+  timerElement.textContent = `${timerCount}`;
 }
-
 
 // The setTimer function starts and stops the timer and triggers winGame() and loseGame()
 function startTimer() {
+  $("#gameOver").css({ display: 'none' })
   // Sets timer
   timerInterval = setInterval(function () {
     timerCount -= 1;
     updateTimerValue();
     if (timerCount <= 0 || currentQuestionIndex === questions.length) {
       gameOver();
-
- 
     }
   }, 1000);
 }
 
-
-function hideIncorrect(){
+function hideIncorrect() {
   $(".incorrect").css({ display: "none" })
 }
 
@@ -65,65 +71,36 @@ function incorrectAnswer() {
   // document.getElementById("startTitle").innerHTML = "INCORRECT";
   timerCount -= 5;
   $(".incorrect").css({ display: "block" })
-  setInterval(hideIncorrect, 2000)
+  setInterval(hideIncorrect, 1000)
 }
 
- 
 function correctAnswer() {
   // document.getElementById("startTitle").innerHTML = "CORRECT!!!🏆 ";
   winCounter++;
   win.textContent = winCounter;
-  console.log("wincounter should ++")
 }
 
 function gameOver() {
-
-	// if( timerCount < 0 ) {
-	// 	timerCount = 0;
-
-  document.getElementById("startTitle").innerHTML = `GAME OVER, refresh page to restart game!
-  score: ${winCounter} time:${timerCount}`;
-  $("#gameOver").css({display: 'block'})
-
+  document.getElementById("startTitle").innerHTML = `GAME OVER!`;
+  document.getElementById("questionsTitle").classList.add("hidden");
+  $("#gameOver").css({ display: 'block' })
   $("#inlineFormInput").css({ display: "block" });
   $(".initials").css({ display: "block" });
   $("#startTitle").css({ display: "block" });
- 
   $("#startTitle").textContent = ("GAME OVER");
   startButton.disabled = false;
+  // disable buttons for game over screen
   btn1.disabled = true;
   btn2.disabled = true;
   btn3.disabled = true;
   btn4.disabled = true;
-  win.textContent=winCounter;
+  win.textContent = winCounter;
   updateTimerValue();
-    
-  clearInterval( timerInterval );
-  
-  console.log("here")
-  // setLosses();
-  //}
+  clearInterval(timerInterval);
 }
-
-var questions = [
-  {
-    question: "What color is a apple?",
-    answers: ["A: red", "B: oragne", "C: yellow", "D: pink"],
-    //correct answers
-    correctAnswer: "A: red",
-  },
-  {
-    question: "what color is a lychee",
-    answers: ["A: brown", "B: pink-red", "C: red-yellow", "D: purple"],
-    //correct answers
-    correctAnswer: "B: pink-red",
-  },
-];
-// Creates blanks on screen
 
 function renderQuestions() {
   questionsTitle.textContent = questions[currentQuestionIndex].question;
-  // for (i = 0; i < questions[currentQuestion].answers.length; i++) {
   btn1.textContent = questions[currentQuestionIndex].answers[0];
   btn2.textContent = questions[currentQuestionIndex].answers[1];
   btn3.textContent = questions[currentQuestionIndex].answers[2];
@@ -139,18 +116,12 @@ for (var i = 0; i < answersBtn.length; i++) {
     ) {
       correctAnswer();
       currentQuestionIndex++;
-      
-      console.log("correct");
     } else {
       incorrectAnswer();
-      console.log("Incorrect minus 5 seconds");
     }
-    console.log(currentQuestionIndex + " is the current question ");
-
-    if (currentQuestionIndex < 2) {
+    if (currentQuestionIndex < 24) {
       renderQuestions();
     } else {
-      // setHighScore();
       gameOver();
       currentQuestionIndex = 0;
       renderQuestions();
@@ -158,54 +129,183 @@ for (var i = 0; i < answersBtn.length; i++) {
   });
 }
 
-
-// var initials = document.querySelector("#initials").value;
-// function setHighScore( winCounter, timerCount,initials ) {
-//  winCounter,
-//  timerCount
-//  initials
-//   // var Highscores = {
-// 	// 	name: initials,
-// 	// 	answerScore: winCounter,
-// 	// 	timeScore: timerCount
-// 	// };
-//   console.log('setHighScore',  winCounter,timerCount,initials)
-//   // var storedHighscores = JSON.parse( localStorage.getItem( 'storedHighscores' ) ) || [];
-
-// 	// storedHighscores.push( storedHighscores );
-
-// 	// localStorage.setItem( 'storedHighscores', JSON.stringify( storedHighscores ) );
-
-// 	// window.location = './Assets/html/highscores.html';
-
-
-//   // console.log("sethigh score")
-  
-// // };
-// var submitInitialsButton = document.querySelector("#submitInitialsButton");
-// var submitInitialsform = document.querySelector("#initialsForm");
-
-document.getElementById("initialsForm").addEventListener("submit", function(event) {
+//event listener
+document.getElementById("initialsForm").addEventListener("submit", function (event) {
   event.preventDefault();
-  
-  // create user object from submission
-  var highscores = {
-   userInitials: initials.value.trim(),
-    wins: winCounter,
-    timeLeft: timerCount+"seconds"
+  const highscoreBtn = document.querySelector('.highscoreBtn')
+  highscoreBtn.classList.remove("hidden")
+  const score = {
+    userInitials: initials.value.trim(),
+    score: winCounter,
+    timeLeft: timerCount
   };
-localStorage.setItem("highscores", JSON.stringify(highscores));
-  console.log(highscores)
-  
-  setInterval(setHighScoreLabel, 2000)
-  // // set new submission to local storage 
-  
-  // console.log(highscores)
+  let worstScore = 0;
+  if (score.score > 4) {
+    worstScore = hiscores[hiscores.length - 1].score;
+  }
+  highscores.push(score)
+  highscores.sort((a, b) => a.score > b.score ? -1 : 1);
+  if (highscores.length > 5) {
+    console.log("highscore length is greater than 5",)
+    highscores.pop();
+    console.log("ORANGES")
+  }
+  localStorage.setItem("highscores", JSON.stringify(highscores));
+  console.log("not even elses", highscores)
 })
- function setHighScoreLabel(){
-  $(".visitHighscore").css({ display: "block" })
- }
 
-
-
-
+// quiz game questions
+var questions = [
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    answers: ["<scripting>", "<js>", "<script>", "<javascript>"],
+    //correct answers
+    correctAnswer: "<script>",
+  },
+  {
+    question: "What is the correct JavaScript syntax to change the content of the HTML element below?",
+    answers: [' #demo.innerHTML = "Hello World!";', ' document.getElement("p").innerHTML = "Hello World!";', ' document.getElementById("demo").innerHTML = "Hello World!";', ' document.getElementByName("p").innerHTML = "Hello World!";'],
+    //correct answers
+    correctAnswer: 'document.getElementById("demo").innerHTML = "Hello World!";',
+  },
+  {
+    question: "Where is the correct place to insert a JavaScript?",
+    answers: [" The <head> section", " Both the <head> section and the <body> section are correct", " The <body> section"],
+    //correct answers
+    correctAnswer: "Both the <head> section and the <body> section are correct",
+  },
+  {
+    question: 'What is the correct syntax for referring to an external script called "xxx.js"?',
+    answers: [' <script name="xxx.js">', ' <script href="xxx.js">', ' <script src="xxx.js">'],
+    //correct answers
+    correctAnswer: '<script src="xxx.js">',
+  },
+  {
+    question: 'The external JavaScript file must contain the <script> tag.',
+    answers: [' True', ' False'],
+    //correct answers
+    correctAnswer: 'False',
+  },
+  {
+    question: 'How do you write "Hello World" in an alert box?',
+    answers: [' alert("Hello World");', '  msgBox("Hello World");', ' msg("Hello World");', ' alertBox("Hello World");'],
+    //correct answers
+    correctAnswer: 'alert("Hello World");',
+  },
+  {
+    question: 'How do you create a function in JavaScript?',
+    answers: ['  function = myFunction()', '  function:myFunction()', ' function myFunction()'],
+    //correct answers
+    correctAnswer: 'function myFunction()',
+  },
+  {
+    question: 'How do you call a function named "myFunction"?',
+    answers: [' call function myFunction()', ' call myFunction()', ' myFunction()'],
+    //correct answers
+    correctAnswer: 'myFunction()',
+  },
+  {
+    question: 'How to write an IF statement in JavaScript?',
+    answers: [' if (i == 5)', ' if i == 5 then', ' if i = 5 then', ' if i = 5'],
+    //correct answers
+    correctAnswer: 'if (i == 5)',
+  },
+  {
+    question: 'How to write an IF statement for executing some code if "i" is NOT equal to 5?',
+    answers: [' if i <> 5', ' if i =! 5 then', ' if (i != 5)', ' if (i <> 5)'],
+    //correct answers
+    correctAnswer: 'if (i != 5)',
+  },
+  {
+    question: 'How does a WHILE loop start?',
+    answers: [' while (i <= 10)', ' while i = 1 to 10', ' while (i <= 10; i++)'],
+    //correct answers
+    correctAnswer: 'while (i <= 10)',
+  },
+  // correct answer may be incorrect
+  {
+    question: 'How does a FOR loop start?',
+    answers: [' for (i = 0; i <= 5)', ' for (i <= 5; i++)', ' for (i = 0; i <= 5; i++)', ' for i = 1 to 5'],
+    //correct answers
+    correctAnswer: 'for (i = 0; i <= 5; i++)',
+  },
+  {
+    question: 'How can you add a comment in a JavaScript?',
+    answers: ["  'This is a comment", ' //This is a comment', ' <!--This is a comment-->'],
+    //correct answers
+    correctAnswer: '//This is a comment',
+  },
+  {
+    question: 'How to insert a comment that has more than one line?',
+    answers: [
+      ` <!--This comment has <br> more than one line-->`, ` //This is a comment <br> more than one line//`, ` /*This comment has <br> more than one line*/`],
+    //correct answers
+    correctAnswer: '//This is a comment',
+  },
+  {
+    question: 'What is the correct way to write a JavaScript array?',
+    answers: [' var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")', ' var colors = "red", "green", "blue"', ' var colors = (1:"red", 2:"green", 3:"blue")', ' var colors = ["red", "green", "blue"]'],
+    //correct answers
+    correctAnswer: '//This is a comment',
+  },
+  {
+    question: 'How do you round the number 7.25, to the nearest integer?',
+    answers: [' round(7.25)', ' rnd(7.25)', ' Math.round(7.25)', ' Math.rnd(7.25)'],
+    //correct answers
+    correctAnswer: 'Math.round(7.25)',
+  },
+  {
+    question: 'How do you find the number with the highest value of x and y?',
+    answers: [' Math.ceil(x, y)', ' Math.max(x, y)', ' top(x, y)', ' ceil(x, y)'],
+    //correct answers
+    correctAnswer: 'Math.max(x, y)',
+  },
+  {
+    question: 'What is the correct JavaScript syntax for opening a new window called "w2" ?',
+    answers: [' w2 = window.new("http://www.w3schools.com");', ' w2 = window.open("http://www.w3schools.com");'],
+    //correct answers
+    correctAnswer: 'w2 = window.open("http://www.w3schools.com");',
+  },
+  {
+    question: 'JavaScript is the same as Java.',
+    answers: [' True', ' False'],
+    //correct answers
+    correctAnswer: 'False',
+  },
+  {
+    question: "How can you detect the client's browser name?",
+    answers: [' navigator.appName', ' browser.name', ' client.navNames'],
+    //correct answers
+    correctAnswer: 'navigator.appName',
+  },
+  {
+    question: "Which event occurs when the user clicks on an HTML element?",
+    answers: [' onchange', ' onmouseover', ' onmouseclick', ' onclick'],
+    //correct answers
+    correctAnswer: 'onclick',
+  },
+  {
+    question: "How do you declare a JavaScript variable?",
+    answers: [' variable carName;', ' var carName;', ' v carName;'],
+    //correct answers
+    correctAnswer: 'var carName;',
+  },
+  {
+    question: "Which operator is used to assign a value to a variable?",
+    answers: [' *', ' =', ' X', ' -'],
+    //correct answers
+    correctAnswer: '=',
+  },
+  {
+    question: "What will the following code return: Boolean(10 > 9)",
+    answers: [' false', ' true', ' NaN'],
+    //correct answers
+    correctAnswer: 'true',
+  },
+  {
+    question: "Is JavaScript case-sensitive?",
+    answers: [' No', ' Yes'],
+    //correct answers
+    correctAnswer: 'Yes',
+  },
+];
